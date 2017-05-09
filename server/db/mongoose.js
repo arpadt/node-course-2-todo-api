f4ecb9ef7b22ad67646cd358d4e30960f3645972
arpadt <a.arpad.toth@gmail.com>
2017-05-09 12:27:24 +0200

    Change database url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://arpadt:gigantic@ds133231.mlab.com:33231/nodetodoapi'
+}
 // mongoose.connect('mongodb://localhost:27017/TodoApp'); // only for local database
-mongoose.connect('mongodb://arpadt:gigantic@ds133231.mlab.com:33231/nodetodoapi');
+mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
 
 module.exports = {mongoose};
