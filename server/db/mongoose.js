458ec5bde6a2114745ffcaa44b593e1e221ff812
arpadt <a.arpad.toth@gmail.com>
2017-05-09 11:02:05 +0200

    Add mlab authentication

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp'); // only for local database
-mongoose.connect('mongodb://arpadt:cUsR5Lkk8h@ds133231.mlab.com:33231/nodetodoapi' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://arpadt:gigantic@ds133231.mlab.com:33231/nodetodoapi' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
