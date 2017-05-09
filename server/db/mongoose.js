04a7a1aa137e3803f1a88cd3c12648ec6af79411
arpadt <a.arpad.toth@gmail.com>
2017-05-09 13:00:12 +0200

    App for heroku again

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://arpadt:gigantic@ds133231.mlab.com:33231/nodetodoapi'
-}
+// let db = {
+//     localhost: 'mongodb://localhost:27017/TodoApp',
+//     mlab: 'mongodb://arpadt:gigantic@ds133231.mlab.com:33231/nodetodoapi'
+// }
 // mongoose.connect('mongodb://localhost:27017/TodoApp'); // only for local database
-mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
+
+// PROD_MONGODB is a var created by heroku when connecting itto mlab
+mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
