df9420a2603f347c37f7916fb1812f2e6505d9ab
arpadt <a.arpad.toth@gmail.com>
2017-05-09 09:42:43 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -30,6 +31,26 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+// GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+    // req.params is an object- key:id, value: whatever the id is
+    let id = req.params.id;
+    // validation
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Invalid id');
+    }
+
+    Todo.findById(id).then( (todo) => {
+        // success - valid id
+            // if todo not found
+            if (!todo) {
+                return res.status(404).send('Todo not found');
+            }
+            // if todo found
+            res.send({todo});
+    }).catch( (e) => res.status(500).send('Server error')); // error, could be status 400
 });
 
 app.listen(3000, () => {
