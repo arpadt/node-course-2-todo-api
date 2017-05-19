6e10741047b2011c8d5ab019faff74c3f9c4b1e4
arpadt <a.arpad.toth@gmail.com>
2017-05-19 18:21:46 +0200

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,11 @@
 let env = process.env.NODE_ENV || 'development'; // environment variable, only set in heroku
 
-// setting two different database, one for deveopment, one for test
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.PROD_MONGODB = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.PROD_MONGODB = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+    let config = require('./config.json');
+    let envConfig = config[env];
+
+    Object.keys(envConfig).forEach( (key) => {
+        process.env[key] = envConfig[key];
+    });
+
 }
